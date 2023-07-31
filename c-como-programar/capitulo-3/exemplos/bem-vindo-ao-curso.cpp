008b14a981c40007c60fea0f8fbd7df6ed732a2d
Eduardo Silva <eduardoalsilva45@gmail.com>
2023-07-31 19:04:21 -0300

    Exercícios do capitulo 3

---
+++ c-como-programar/capitulo-3/exemplos/bem-vindo-ao-curso.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+#include <string>
+using std::string;
+using std::getline;
+
+class GradeBook
+{
+public:
+  void displayMessage( string courseName)
+  {
+    cout << "Welcome to the grade book for \n" << courseName << endl << "!";
+  }
+
+};
+
+int main(){
+  string nameOfCourse;
+  GradeBook myGradeBook;
+
+  cout << "Enter the name of the course:" << endl;
+  getline(cin, nameOfCourse);
+  cout << endl;
+
+  myGradeBook.displayMessage(nameOfCourse);
+
+  return 0;
+
+}
+  
